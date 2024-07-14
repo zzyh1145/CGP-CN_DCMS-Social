b77ceaac916a3c9f62583b28790397f708c30d56
Citruy_404 <115987096+zzyh1145@users.noreply.github.com>
2024-07-14 17:24:18 +0800

    修改db_connect.php的路径

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once '../sys/inc/compress.php';
 include_once '../sys/inc/sess.php';
 include_once '../sys/inc/home.php';
 include_once '../sys/inc/settings.php';
-include_once '../sys/inc/db_connect.php';
+include_once '../group/db_connect.php';
 include_once '../sys/inc/ipua.php';
 include_once '../sys/inc/fnc.php';
 include_once '../sys/inc/user.php';
